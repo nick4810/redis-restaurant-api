69c4a07ddac5b79191fcdb12bbd0ccb8acf107c0
Nick Sandstrom <32273437+nick4810@users.noreply.github.com>
2026-07-06 23:59:20 -0700

    Added initial Cuisine relationship

--- utils/redisKeys.ts
+++ utils/redisKeys.ts
@@ -5,3 +5,6 @@ export const getKeyName = (...args: string[]): string => {
 export const restaurantKeyById = (id: string) => getKeyName('restaurants', id);
 export const reviewKeyById = (id: string) => getKeyName('reviews', id);
 export const reviewDetailsKeyById = (id: string) => getKeyName('review-details', id);
+export const cuisinesKey = getKeyName('cuisines');
+export const cuisineKey = (name: string) => getKeyName('cuisines', name);
+export const restaurantCuisinesKeyById = (id: string) => getKeyName('restaurant_cuisines', id);
